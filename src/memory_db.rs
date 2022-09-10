dac64e7499d2d7a3f4f5f5fc5fe020316f5e357b
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-09-10 01:49:47 -0400

    access tracking?

--- src/memory_db.rs
+++ src/memory_db.rs
@@ -7,6 +7,7 @@ use super::lattice::{Semilattice, LatticeReadDB, LatticeWriteDB};
 pub struct MemoryLatticeDB<L: Semilattice> {
     lattice: Arc<L>,
     maxes: Mutex<BTreeMap<L::LID, L::Value>>,
+    accesses: Mutex<Vec<Vec<L::LID>>>,
 }
 
 impl<L: Semilattice> MemoryLatticeDB<L> {
@@ -14,7 +15,22 @@ impl<L: Semilattice> MemoryLatticeDB<L> {
         MemoryLatticeDB {
             lattice,
             maxes: Mutex::new(BTreeMap::new()),
+            accesses: Mutex::new(Vec::new()),
         }
+    }
+    fn push_accesses(&self) {
+        let mut accesses = self.accesses.lock().unwrap();
+        accesses.push(Vec::new());
+    }
+    fn add_access(&self, lid: L::LID) {
+        let mut accesses = self.accesses.lock().unwrap();
+        for acc_list in accesses.iter_mut() {
+            acc_list.push(lid.clone());
+        }
+    }
+    fn pop_accesses(&self) -> Vec<L::LID> {
+        let mut accesses = self.accesses.lock().unwrap();
+        accesses.pop().unwrap()
     }
 }
 
@@ -24,6 +40,7 @@ impl<L: Semilattice + 'static> LatticeReadDB<L> for MemoryLatticeDB<L> {
         self.lattice.clone()
     }
     async fn get_lattice_max(self: Arc<Self>, lid: L::LID) -> Option<L::Value> {
+        self.add_access(lid.clone());
         self.maxes.lock().unwrap().get(&lid).cloned()
     }
 }
@@ -37,7 +54,9 @@ impl<L: Semilattice + 'static> LatticeWriteDB<L> for MemoryLatticeDB<L> {
     async fn put_lattice_value(self: Arc<Self>, lid: L::LID, value: L::Value) -> Result<(), String> {
         let default = self.lattice.default(lid.clone())?;
         let current = self.clone().get_lattice_max(lid.clone()).await.unwrap_or(default);
+        self.push_accesses();
         let joined = self.lattice.clone().join(lid.clone(), self.clone(), current.clone(), value).await?;
+        let accesses = self.pop_accesses();
         if joined != current {
             self.maxes.lock().unwrap().insert(lid, joined);
         }
