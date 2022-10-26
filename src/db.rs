de8293e926c50d1ecd1c4e4def5c83b89fd3f931
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-10-26 18:07:04 -0400

    db types

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,7 @@
 use std::sync::Arc;
 
+use core::fmt::{Debug, Formatter};
+
 use async_trait::async_trait;
 
 use crate::crypto::{HashCode, hash_of_bytes};
@@ -7,49 +9,47 @@ use crate::tagged_mapping::TaggedMapping;
 use crate::lattice::{LatticeLibrary, ComputationLibrary, ImmutComputationContext, MutComputationContext, LatticeContext};
 use serde::{Serialize, Deserialize, de::DeserializeOwned};
 
-pub trait LatDB<CI: TaggedMapping, L: TaggedMapping> : Send + Sync {
+pub trait DepDB<M: TaggedMapping>: Send + Sync {
 
-    fn get_hash(&self, key: HashCode) -> Result<Option<Vec<u8>>, String>;
-    fn put_hash(&mut self, key: HashCode, value: HashCode) -> Result<(), String>;
-    fn remove_hash(&mut self, key: HashCode) -> Result<(), String>;
+    fn get_value(&self, key: &M::Key) -> Result<Option<M::Value>, String>;
 
-    fn get_immut(&self, key: &CI::Key) -> Result<Option<CI::Value>, String>;
-    fn put_immut(&mut self, key: CI::Key, value: CI::Value) -> Result<(), String>;
-    fn remove_immut(&mut self, key: &CI::Key) -> Result<(), String>;
+    fn set_value_deps(&self, key: M::Key, value: M::Value, deps: Vec<M::Key>) -> Result<(), String>;
 
-    fn get_immut_hash_deps(&self, key: &CI::Key) -> Result<Vec<HashCode>, String>;
-    fn put_immut_hash_deps(&mut self, key: CI::Key, deps: HashCode) -> Result<(), String>;
-    fn remove_immut_hash_dep(&mut self, key: &CI::Key) -> Result<(), String>;
+    fn clear_value_deps(&self, key: &M::Key) -> Result<(), String>;
+}
 
-    fn depless_hashes(&self) -> Result<Vec<HashCode>, String>;
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug, Serialize, Deserialize)]
+pub enum LatDBKey<IK, LK> {
+    Hash(HashCode),
+    Immut(IK),
+    Lattice(LK),
+}
 
-    fn get_immut_immut_deps(&self, key: &CI::Key) -> Result<Vec<CI::Key>, String>;
-    fn put_immut_immut_dep(&mut self, key: CI::Key, dep: CI::Key) -> Result<(), String>;
-    fn remove_immut_immut_dep(&mut self, key: &CI::Key, dep: &CI::Key) -> Result<(), String>;
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug, Serialize, Deserialize)]
+pub enum LatDBValue<IV, LV> {
+    Hash(Vec<u8>),
+    Immut(IV),
+    Lattice(LV),
+}
 
-    fn depless_immuts(&self) -> Result<Vec<CI::Key>, String>;
+pub struct LatDBMapping<CI: TaggedMapping, L: TaggedMapping> {
+    immut: CI,
+    lat: L
+}
 
-    fn get_lat(&self, key: &L::Key) -> Result<Option<L::Value>, String>;
-    fn put_lat(&mut self, key: L::Key, value: L::Value) -> Result<(), String>;
-    fn remove_lat(&mut self, key: &L::Key) -> Result<(), String>;
-
-    fn get_lat_hash_deps(&self, key: &L::Key) -> Result<Vec<HashCode>, String>;
-    fn put_lat_hash_dep(&mut self, key: L::Key, dep: HashCode) -> Result<(), String>;
-    fn remove_lat_Hash_dep(&mut self, key: &L::Key, dep: HashCode) -> Result<(), String>;
-
-    fn get_lat_immut_deps(&self, key: &L::Key) -> Result<Vec<CI::Key>, String>;
-    fn put_lat_immut_dep(&mut self, key: L::Key, dep: CI::Key) -> Result<(), String>;
-    fn remove_lat_immut_dep(&mut self, key: &L::Key, dep: &CI::Key) -> Result<(), String>;
+impl<CI: TaggedMapping, L: TaggedMapping> TaggedMapping for LatDBMapping<CI, L> {
+    type Key = LatDBKey<CI::Key, L::Key>;
+    type Value = LatDBValue<CI::Value, L::Value>;
 }
 
 pub struct LatStore<CI: TaggedMapping, L: TaggedMapping> {
-    db: Box<dyn LatDB<CI, L>>,
+    db: Box<dyn DepDB<LatDBMapping<CI, L>>>,
     comp_lib: Arc<dyn ComputationLibrary<CI>>,
     lat_lib: Arc<dyn LatticeLibrary<CI, L>>,
 }
 
 impl<CI: TaggedMapping, L: TaggedMapping> LatStore<CI, L> {
-    pub fn new(db: impl LatDB<CI, L> + 'static,
+    pub fn new(db: impl DepDB<LatDBMapping<CI, L>> + 'static,
                comp_lib: impl ComputationLibrary<CI> + 'static,
                lat_lib: impl LatticeLibrary<CI, L> + 'static) -> Self {
         Self { db: Box::new(db), comp_lib: Arc::new(comp_lib), lat_lib: Arc::new(lat_lib) }
@@ -58,15 +58,19 @@ impl<CI: TaggedMapping, L: TaggedMapping> LatStore<CI, L> {
 
 impl<CI: TaggedMapping, L: TaggedMapping> ImmutComputationContext<CI> for LatStore<CI, L> {
     fn hash_lookup(&mut self, hash: HashCode) -> Result<Vec<u8>, String> {
-        self.db.get_hash(hash)?.ok_or_else(|| format!("Hash not found: {:?}", hash))
+        match self.db.get_value(&LatDBKey::Hash(hash))? {
+            Some(LatDBValue::Hash(bytes)) => Ok(bytes),
+            _ => Err(format!("Hash not found: {:?}", hash)),
+        }
     }
 
     fn eval_immut(&mut self, key: &CI::Key) -> Result<CI::Value, String> {
-        if let Some(value) = self.db.get_immut(key)? {
+        let immut_key = LatDBKey::Immut(key.clone());
+        if let Some(LatDBValue::Immut(value)) = self.db.get_value(&immut_key)? {
             return Ok(value);
         }
         let res = self.comp_lib.clone().eval_immut(key, self)?;
-        self.db.put_immut(key.clone(), res.clone());
+        self.db.set_value_deps(immut_key, LatDBValue::Immut(res.clone()), vec![]);
         Ok(res)
     }
 }
