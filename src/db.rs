5d12454fce0e73883e8d6891a996dcbbe19c1652
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-10-26 18:10:37 -0400

    dependency tracking!

--- src/db.rs
+++ src/db.rs
@@ -46,18 +46,27 @@ pub struct LatStore<CI: TaggedMapping, L: TaggedMapping> {
     db: Box<dyn DepDB<LatDBMapping<CI, L>>>,
     comp_lib: Arc<dyn ComputationLibrary<CI>>,
     lat_lib: Arc<dyn LatticeLibrary<CI, L>>,
+    deps_stack: Vec<Vec<LatDBKey<CI::Key, L::Key>>>,
 }
 
 impl<CI: TaggedMapping, L: TaggedMapping> LatStore<CI, L> {
     pub fn new(db: impl DepDB<LatDBMapping<CI, L>> + 'static,
                comp_lib: impl ComputationLibrary<CI> + 'static,
                lat_lib: impl LatticeLibrary<CI, L> + 'static) -> Self {
-        Self { db: Box::new(db), comp_lib: Arc::new(comp_lib), lat_lib: Arc::new(lat_lib) }
+        Self {
+            db: Box::new(db),
+            comp_lib: Arc::new(comp_lib),
+            lat_lib: Arc::new(lat_lib),
+            deps_stack: Vec::new(),
+        }
     }
 }
 
 impl<CI: TaggedMapping, L: TaggedMapping> ImmutComputationContext<CI> for LatStore<CI, L> {
     fn hash_lookup(&mut self, hash: HashCode) -> Result<Vec<u8>, String> {
+        if let Some(deps) = self.deps_stack.last_mut() {
+            deps.push(LatDBKey::Hash(hash));
+        }
         match self.db.get_value(&LatDBKey::Hash(hash))? {
             Some(LatDBValue::Hash(bytes)) => Ok(bytes),
             _ => Err(format!("Hash not found: {:?}", hash)),
@@ -65,12 +74,24 @@ impl<CI: TaggedMapping, L: TaggedMapping> ImmutComputationContext<CI> for LatSto
     }
 
     fn eval_immut(&mut self, key: &CI::Key) -> Result<CI::Value, String> {
+        if let Some(deps) = self.deps_stack.last_mut() {
+            deps.push(LatDBKey::Immut(key.clone()));
+        }
         let immut_key = LatDBKey::Immut(key.clone());
         if let Some(LatDBValue::Immut(value)) = self.db.get_value(&immut_key)? {
             return Ok(value);
         }
-        let res = self.comp_lib.clone().eval_immut(key, self)?;
-        self.db.set_value_deps(immut_key, LatDBValue::Immut(res.clone()), vec![]);
-        Ok(res)
+        self.deps_stack.push(Vec::new());
+        match self.comp_lib.clone().eval_immut(key, self) {
+            Err(err) => {
+                self.deps_stack.pop();
+                Err(err)
+            }
+            Ok(value) => {
+                let deps = self.deps_stack.pop().unwrap();
+                self.db.set_value_deps(immut_key, LatDBValue::Immut(value.clone()), deps)?;
+                Ok(value)
+            }
+        }
     }
 }
