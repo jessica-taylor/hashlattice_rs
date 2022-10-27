dcc1560bd8a49a800a866c93b03bf37440c61b38
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-10-27 17:55:13 -0400

    rsa signatures!

--- src/crypto.rs
+++ src/crypto.rs
@@ -5,23 +5,24 @@ use std::convert::TryInto;
 use std::default::Default;
 use std::marker::PhantomData;
 
-// use ed25519_dalek::{Signer, Verifier};
-use rand::prelude::*;
+use rand::thread_rng;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-
-/* QUESTION: tell me more about how much of this is table stakes
-for a generic implementation, how much of it is reusable with
-cosmos, and how much of it is somewhere in between? */
+use rsa::pkcs1v15::{SigningKey, VerifyingKey};
+use rsa::pkcs1v15::Signature as RsaSignature;
+use rsa::{RsaPrivateKey, RsaPublicKey};
+use rsa::PublicKey as RsaPublicKeyTrait;
+use signature::{Signer, Verifier, DigestVerifier};
+use signature::Signature as SignatureTrait;
 
 /// A SHA256 hash code.
 pub type HashCode = [u8; 32];
 
-// pub type PublicKey = ed25519_dalek::PublicKey;
-// 
-// pub type Keypair = ed25519_dalek::Keypair;
-// 
-// pub type Signature = ed25519_dalek::Signature;
+pub type PublicKey = RsaPublicKey;
+
+pub type Keypair = RsaPrivateKey;
+
+pub type Signature = Vec<u8>;
 
 /// A hash code that is tagged as being a hash code of a particular serializable type.
 #[derive(Serialize, Deserialize, Debug)]
@@ -118,78 +119,82 @@ pub fn hash<T: Serialize>(v: &T) -> Hash<T> {
     }
 }
 
-// /// An ed25519 signature that is tagged as being the signature of a particular serializable type.
-// #[derive(Serialize, Deserialize, Debug)]
-// pub struct Sig<T> {
-//     /// The signature.
-//     pub signature: Signature,
-//     /// The public key.
-//     pub key: PublicKey,
-//     /// Phantom data for the type `T`.
-//     phantom: std::marker::PhantomData<T>,
-// }
-// 
-// impl<T> Sig<T> {
-//     pub fn account(&self) -> Account {
-//         hash(&self.key)
-//     }
-// }
-// 
-// impl<T> Clone for Sig<T> {
-//     fn clone(&self) -> Self {
-//         Self {
-//             signature: self.signature,
-//             key: self.key,
-//             phantom: PhantomData,
-//         }
-//     }
-// }
-// 
-// impl<T> Copy for Sig<T> {}
-// 
-// impl<T> PartialEq for Sig<T> {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.signature == other.signature
-//     }
-// }
-// 
-// impl<T> Eq for Sig<T> {}
-// 
-// impl<T> PartialOrd for Sig<T> {
-//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//         rmp_serde::to_vec_named(self)
-//             .unwrap()
-//             .partial_cmp(&rmp_serde::to_vec_named(other).unwrap())
-//     }
-// }
-// 
-// impl<T> Ord for Sig<T> {
-//     fn cmp(&self, other: &Self) -> Ordering {
-//         rmp_serde::to_vec_named(self)
-//             .unwrap()
-//             .cmp(&rmp_serde::to_vec_named(other).unwrap())
-//     }
-// }
-// 
-// /// Generates a ed25519 private key.
-// pub fn gen_private_key() -> Keypair {
-//     Keypair::generate(&mut thread_rng())
-// }
-// 
-// /// Signs a serializable with a given ed25519 key.
-// pub fn sign<T: Serialize>(key: &Keypair, msg: T) -> Sig<T> {
-//     Sig {
-//         signature: key.sign(&rmp_serde::to_vec_named(&msg).unwrap()),
-//         key: key.public,
-//         phantom: std::marker::PhantomData,
-//     }
-// }
-// 
-// impl<T: Serialize> Sig<T> {
-//     /// Verifies that a given signature of a given serializable is valid.
-//     pub fn verify(&self, msg: &T) -> bool {
-//         self.key
-//             .verify(&rmp_serde::to_vec_named(msg).unwrap(), &self.signature)
-//             .is_ok()
-//     }
-// }
+/// An ed25519 signature that is tagged as being the signature of a particular serializable type.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Sig<T> {
+    /// The signature.
+    pub signature: Signature,
+    /// The public key.
+    pub key: PublicKey,
+    /// Phantom data for the type `T`.
+    phantom: std::marker::PhantomData<T>,
+}
+
+impl<T> Sig<T> {
+    // pub fn account(&self) -> Account {
+    //     hash(&self.key)
+    // }
+}
+
+impl<T> Clone for Sig<T> {
+    fn clone(&self) -> Self {
+        Self {
+            signature: self.signature.clone(),
+            key: self.key.clone(),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> PartialEq for Sig<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.signature == other.signature
+    }
+}
+
+impl<T> Eq for Sig<T> {}
+
+impl<T> PartialOrd for Sig<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        rmp_serde::to_vec_named(self)
+            .unwrap()
+            .partial_cmp(&rmp_serde::to_vec_named(other).unwrap())
+    }
+}
+
+impl<T> Ord for Sig<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        rmp_serde::to_vec_named(self)
+            .unwrap()
+            .cmp(&rmp_serde::to_vec_named(other).unwrap())
+    }
+}
+
+/// Generates a ed25519 private key.
+pub fn gen_private_key() -> Keypair {
+    let mut rng = rand::thread_rng();
+    let bits = 2048;
+    Keypair::new(&mut rng, bits).expect("failed to generate keypair")
+}
+
+/// Signs a serializable with a given ed25519 key.
+pub fn sign<T: Serialize>(key: &Keypair, msg: T) -> Sig<T> {
+    let sign_key = SigningKey::<Sha256>::new(key.clone());
+    let sig = sign_key.sign(&rmp_serde::to_vec_named(&msg).unwrap());
+    Sig {
+        signature: sig.to_vec(),
+        key: key.to_public_key(),
+        phantom: std::marker::PhantomData,
+    }
+}
+
+impl<T: Serialize> Sig<T> {
+    /// Verifies that a given signature of a given serializable is valid.
+    pub fn verify(&self, msg: &T) -> bool {
+        let verify_key: VerifyingKey<_> = VerifyingKey::<Sha256>::new(self.key.clone());
+        let sig = RsaSignature::from(self.signature.clone());
+        verify_key
+            .verify(&rmp_serde::to_vec_named(msg).unwrap(), &sig)
+            .is_ok()
+    }
+}
