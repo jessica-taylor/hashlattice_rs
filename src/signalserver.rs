ca5984999901060bb76756e3bbdad2e47f554280
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-10-27 16:37:06 -0400

    websocket stream

--- src/signalserver.rs
+++ src/signalserver.rs
@@ -1,6 +1,12 @@
 use std::net::SocketAddr;
-use tokio::net::{TcpListener, TcpStream};
+
 use futures::{future, StreamExt, TryStreamExt};
+
+use tokio::net::{TcpListener, TcpStream};
+use tungstenite::protocol::Message;
+use tokio_tungstenite::WebSocketStream;
+
+// https://github.com/snapview/tokio-tungstenite/blob/master/examples/autobahn-server.rs
 
 async fn main() -> Result<(), String> {
     // let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
@@ -21,16 +27,21 @@ async fn accept_connection(stream: TcpStream) {
     let addr = stream.peer_addr().expect("connected streams should have a peer address");
     println!("Peer address: {}", addr);
 
-    let ws_stream = tokio_tungstenite::accept_async(stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
 
     println!("New WebSocket connection: {}", addr);
 
-    let (write, read) = ws_stream.split();
-    // We should not forward messages other than text or binary.
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .forward(write)
+    if let Err(e) = handle_connection(addr, stream).await {
+        println!("Error handling websocket connection: {}", e);
+    }
+}
+
+async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<(), String> {
+    let mut ws_stream = tokio_tungstenite::accept_async(stream)
         .await
-        .expect("Failed to forward messages")
+        .expect("Error during the websocket handshake occurred");
+    while let Some(msg) = ws_stream.next().await {
+        let msg = msg.map_err(|e| e.to_string())?;
+        println!("Received a message from {}: {}", peer, msg);
+    }
+    Ok(())
 }
