eeba1d8fc9e40f7187dd60222d2009a7daafb35a
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-10-29 17:34:31 -0400

    cargo fix

--- tests/test_db.rs
+++ tests/test_db.rs
@@ -1,7 +1,7 @@
 use hashlattice::error::{Res, str_error};
 use hashlattice::tagged_mapping::TaggedMapping;
-use hashlattice::lattice::{HashLookup, ComputationLibrary, EmptyComputationLibrary, ComputationImmutContext, ComputationMutContext, LatticeLibrary, EmptyLatticeLibrary, LatticeImmutContext, LatticeMutContext, EmptyContext};
-use hashlattice::db::{DepDB, LatStore, LatDBMapping};
+use hashlattice::lattice::{EmptyComputationLibrary, LatticeLibrary, LatticeImmutContext, LatticeMutContext, EmptyContext};
+use hashlattice::db::{LatStore, LatDBMapping};
 use hashlattice::sql_db::SqlDepDB;
 
 struct EmptyMapping;
@@ -19,20 +19,20 @@ impl TaggedMapping for MaxTupleMapping {
 struct MaxTupleLatLibrary(usize);
 
 impl LatticeLibrary<EmptyMapping, MaxTupleMapping, EmptyMapping> for MaxTupleLatLibrary {
-    fn check_elem(&self, key: &String, value: &Vec<usize>, ctx: &dyn LatticeImmutContext<EmptyMapping, MaxTupleMapping, EmptyMapping>) -> Res<()> {
+    fn check_elem(&self, _key: &String, value: &Vec<usize>, _ctx: &dyn LatticeImmutContext<EmptyMapping, MaxTupleMapping, EmptyMapping>) -> Res<()> {
         if value.len() != self.0 {
             return str_error("wrong length");
         }
         Ok(())
     }
-    fn join(&self, key: &String, value1: &Vec<usize>, value2: &Vec<usize>, ctx: &dyn LatticeMutContext<EmptyMapping, MaxTupleMapping, EmptyMapping>) -> Res<Vec<usize>> {
+    fn join(&self, _key: &String, value1: &Vec<usize>, value2: &Vec<usize>, _ctx: &dyn LatticeMutContext<EmptyMapping, MaxTupleMapping, EmptyMapping>) -> Res<Vec<usize>> {
         let mut result = value1.clone();
         for (i, v) in value2.iter().enumerate() {
             result[i] = std::cmp::max(result[i], *v);
         }
         Ok(result)
     }
-    fn eval_lat_computation(&self, comp: &(), ctx: &dyn LatticeImmutContext<EmptyMapping, MaxTupleMapping, EmptyMapping>) -> Res<()> {
+    fn eval_lat_computation(&self, _comp: &(), _ctx: &dyn LatticeImmutContext<EmptyMapping, MaxTupleMapping, EmptyMapping>) -> Res<()> {
         str_error("no computations")
     }
 
@@ -43,7 +43,7 @@ impl LatticeLibrary<EmptyMapping, MaxTupleMapping, EmptyMapping> for MaxTupleLat
 async fn test_db() -> Res<()> {
     let mut db = SqlDepDB::<LatDBMapping<EmptyMapping, MaxTupleMapping, EmptyMapping>>::new(":memory:").unwrap();
     db.initialize().unwrap();
-    let mut store = LatStore::new(db, EmptyComputationLibrary, MaxTupleLatLibrary(3));
+    let store = LatStore::new(db, EmptyComputationLibrary, MaxTupleLatLibrary(3));
     let mut key = "first".to_string();
     assert!(store.lattice_lookup(&key)?.is_none());
     assert_eq!(vec![1, 2, 0], store.lattice_join(&key, &vec![1, 2, 0], &EmptyContext).unwrap());
