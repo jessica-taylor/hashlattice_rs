b6fd71bd2a1066100812a22850bfeddb53456607
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-11-02 01:50:17 -0400

    poll events pending!!

--- src/js/runtime_channel.rs
+++ src/js/runtime_channel.rs
@@ -1,10 +1,13 @@
 
+use std::ops::DerefMut;
 use std::collections::BTreeMap;
 use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{channel, Sender, Receiver};
+use std::sync::mpsc::{channel, Sender, Receiver, TryRecvError};
 use std::rc::Rc;
 use core::cmp::Ordering;
+use core::pin::Pin;
 
+use futures::task::{Poll, Context};
 use futures::Future;
 use futures::future::{FutureExt, poll_fn};
 use futures::channel::oneshot;
@@ -156,12 +159,13 @@ async fn op_lattice_join(state: &mut OpState, globalid: u32, ctxid: CtxId, key: 
 }
 
 
+#[derive(Clone)]
 struct RuntimeState {
     runtime: Arc<Mutex<JsRuntime>>,
     script: String,
     global_id: u32,
-    receiver: Receiver<MessageToRuntime>,
-    library_futures: BTreeMap<QueryId, Box<dyn Future<Output = Res<LibraryResult>>>>,
+    receiver: Arc<Receiver<MessageToRuntime>>,
+    library_futures: Arc<Mutex<BTreeMap<QueryId, Pin<Box<dyn Future<Output = Res<LibraryResult>>>>>>>,
 }
 
 impl RuntimeState {
@@ -193,9 +197,9 @@ impl RuntimeState {
         (Self {
             runtime: Arc::new(Mutex::new(runtime)),
             script,
-            receiver: to_runtime_receiver,
+            receiver: Arc::new(to_runtime_receiver),
             global_id,
-            library_futures: BTreeMap::new(),
+            library_futures: Arc::new(Mutex::new(BTreeMap::<QueryId, Pin<Box<dyn Future<Output = Res<LibraryResult>>>>>::new())),
         }, to_runtime_sender, from_runtime_receiver)
     }
     async fn call_function(runtime_arc: Arc<Mutex<JsRuntime>>, path: String, args: Vec<JsValue>) -> Result<JsValue, AnyError> {
@@ -218,43 +222,89 @@ impl RuntimeState {
             })
         }).await
     }
-    fn register_call_function(&mut self, query_id: QueryId, path: &str, args: Vec<JsValue>, res_handler: impl 'static + FnOnce(JsValue) -> Res<LibraryResult>) {
+    fn register_call_function(&self, query_id: QueryId, path: &str, args: Vec<JsValue>, res_handler: impl 'static + FnOnce(JsValue) -> Res<LibraryResult>) {
         let fut = Self::call_function(self.runtime.clone(), path.to_string(), args).map(|res| res_handler(res?));
-        self.library_futures.insert(query_id, Box::new(fut));
+        self.library_futures.lock().unwrap().insert(query_id, Box::pin(fut));
     }
-    pub fn process_messages(&mut self) {
-        while let Ok(msg) = self.receiver.try_recv() {
-            match msg {
-                MessageToRuntime::LibraryQuery(query_id, query) => {
-                    match query {
-                        LibraryQuery::EvalComputation(key, ctxid) => {
-                            self.register_call_function(query_id, "eval_computation", vec![key, JsValue::from(ctxid)], |res| Ok(LibraryResult::EvalComputation(res)));
-                        },
-                        LibraryQuery::CheckElem(key, value, ctxid) => {
-                            self.register_call_function(query_id, "hash_put", vec![key, value, JsValue::from(ctxid)], |res| Ok(LibraryResult::CheckElem));
-                        },
-                        LibraryQuery::Join(key, value1, value2, ctxid) => {
-                            self.register_call_function(query_id, "lattice_join", vec![key, value1, value2, JsValue::from(ctxid)], |res| Ok(LibraryResult::Join(res)));
-                        },
-                        LibraryQuery::Bottom(key) => {
-                            self.register_call_function(query_id, "lattice_lookup", vec![key], |res| Ok(LibraryResult::Bottom(res)));
-                        },
-                        LibraryQuery::Transport(key, value, old_ctxid, new_ctxid) => {
-                            self.register_call_function(query_id, "eval_lat_computation", vec![key, value, JsValue::from(old_ctxid), JsValue::from(new_ctxid)], |res| Ok(LibraryResult::Transport(res)));
-                        },
-                        LibraryQuery::EvalLatComputation(key, ctxid) => {
-                            self.register_call_function(query_id, "eval_lat_computation", vec![key, JsValue::from(ctxid)], |res| Ok(LibraryResult::EvalLatComputation(res)));
-                        },
+    fn poll_events_pending(&self, ctx: &mut Context<'_>) -> Res<bool> {
+        let mut is_pending = false;
+        if self.runtime.lock().unwrap().poll_event_loop(ctx, false).is_pending() {
+            is_pending = true;
+        }
+        {
+            let mut library_futures = self.library_futures.lock().unwrap();
+            let mut to_remove = Vec::new();
+            for (query_id, mut fut) in library_futures.iter_mut() {
+                match Pin::new(fut.deref_mut()).poll(ctx) {
+                    Poll::Ready(res) => {
+                        let global = self.runtime.lock().unwrap().op_state().borrow_mut().resource_table.get::<GlobalResource>(self.global_id).unwrap();
+                        global.sender.send(MessageFromRuntime::LibraryResult(*query_id, res))?;
+                        to_remove.push(*query_id);
                     }
-                },
-                MessageToRuntime::CtxResult(query_id, result) => {
-                    let global = self.runtime.lock().unwrap().op_state().borrow_mut().resource_table.get::<GlobalResource>(self.global_id).unwrap();
-                    let mut query_state = global.query_state.lock().unwrap();
-                    let query_receiver = query_state.query_receivers.remove(&query_id).unwrap();
-                    query_receiver.send(result).unwrap();
-                },
+                    Poll::Pending => {
+                        is_pending = true;
+                    }
+                }
+            }
+            for query_id in to_remove {
+                library_futures.remove(&query_id);
             }
         }
+        match self.receiver.try_recv() {
+            Err(TryRecvError::Empty) => {
+                is_pending = true;
+            },
+            Err(TryRecvError::Disconnected) => {},
+            Ok(msg) => {
+                is_pending = true;
+                match msg {
+                    MessageToRuntime::LibraryQuery(query_id, query) => {
+                        match query {
+                            LibraryQuery::EvalComputation(key, ctxid) => {
+                                self.register_call_function(query_id, "eval_computation", vec![key, JsValue::from(ctxid)], |res| Ok(LibraryResult::EvalComputation(res)));
+                            },
+                            LibraryQuery::CheckElem(key, value, ctxid) => {
+                                self.register_call_function(query_id, "hash_put", vec![key, value, JsValue::from(ctxid)], |res| Ok(LibraryResult::CheckElem));
+                            },
+                            LibraryQuery::Join(key, value1, value2, ctxid) => {
+                                self.register_call_function(query_id, "lattice_join", vec![key, value1, value2, JsValue::from(ctxid)], |res| Ok(LibraryResult::Join(res)));
+                            },
+                            LibraryQuery::Bottom(key) => {
+                                self.register_call_function(query_id, "lattice_lookup", vec![key], |res| Ok(LibraryResult::Bottom(res)));
+                            },
+                            LibraryQuery::Transport(key, value, old_ctxid, new_ctxid) => {
+                                self.register_call_function(query_id, "eval_lat_computation", vec![key, value, JsValue::from(old_ctxid), JsValue::from(new_ctxid)], |res| Ok(LibraryResult::Transport(res)));
+                            },
+                            LibraryQuery::EvalLatComputation(key, ctxid) => {
+                                self.register_call_function(query_id, "eval_lat_computation", vec![key, JsValue::from(ctxid)], |res| Ok(LibraryResult::EvalLatComputation(res)));
+                            },
+                        }
+                    },
+                    MessageToRuntime::CtxResult(query_id, result) => {
+                        let global = self.runtime.lock().unwrap().op_state().borrow_mut().resource_table.get::<GlobalResource>(self.global_id).unwrap();
+                        let mut query_state = global.query_state.lock().unwrap();
+                        let query_receiver = query_state.query_receivers.remove(&query_id).unwrap();
+                        query_receiver.send(result).unwrap();
+                    },
+                }
+            }
+        }
+        Ok(is_pending)
+    }
+    pub async fn process_messages(&mut self) -> Res<()> {
+        let self2 = self.clone();
+        poll_fn(move |ctx| {
+            match self2.poll_events_pending(ctx) {
+                Ok(is_pending) => {
+                    if is_pending {
+                        Poll::Pending
+                    } else {
+                        Poll::Ready(Ok(()))
+                    }
+                }
+                Err(err) => Poll::Ready(Err(err)),
+            }
+        }).await
     }
 }
 
