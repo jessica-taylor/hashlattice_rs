a1f4ee53ac7370e5de056e678c61f387d5ede1d5
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-11-04 23:59:34 -0400

    webrtc struct

--- src/transport/webrtc.rs
+++ src/transport/webrtc.rs
@@ -1,82 +1,92 @@
 use std::sync::Arc;
 
 use webrtc::api::APIBuilder;
+use webrtc::data_channel::RTCDataChannel;
 use webrtc::data_channel::data_channel_message::DataChannelMessage;
+use webrtc::peer_connection::RTCPeerConnection;
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
 use webrtc::peer_connection::configuration::RTCConfiguration;
 use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::ice_transport::ice_candidate::RTCIceCandidateInit;
 use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
 
 use crate::error::Res;
 
 // https://github.com/webrtc-rs/webrtc/blob/master/examples/examples/data-channels-create/data-channels-create.rs
 
-async fn test() -> Res<()> {
-    let api = APIBuilder::new().build();
+trait RTCSignalClient : Send + Sync {
+    fn send_session_description(&self, sdp: RTCSessionDescription);
+    fn on_remote_session_description(&self, fun: Box<dyn Fn(RTCSessionDescription)>);
+    fn send_ice_candidate(&self, candidate: RTCIceCandidateInit);
+    fn on_remote_ice_candidate(&self, fun: Box<dyn Fn(RTCIceCandidateInit)>);
+}
 
-    let config = RTCConfiguration {
-        ice_servers: vec![RTCIceServer {
-            urls: vec!["stun:stun.l.google.com:19302".to_string()],
+struct PeerConnection {
+    signal_client: Arc<dyn RTCSignalClient>,
+    rtc_connection: RTCPeerConnection,
+}
+
+impl PeerConnection {
+    async fn new(signal_client: Arc<dyn RTCSignalClient>) -> Res<Self> {
+        let api = APIBuilder::new().build();
+
+        let config = RTCConfiguration {
+            ice_servers: vec![RTCIceServer {
+                urls: vec!["stun:stun.l.google.com:19302".to_string()],
+                ..Default::default()
+            }],
             ..Default::default()
-        }],
-        ..Default::default()
-    };
-    let peer_connection = api.new_peer_connection(config).await?;
+        };
+        let rtc_connection = api.new_peer_connection(config).await?;
+        Ok(Self {signal_client, rtc_connection})
+    }
+    async fn initialize(&mut self) -> Res<()> {
 
-    let data_channel = peer_connection.create_data_channel("data", None).await?;
-
-    peer_connection
-        .on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
+        self.rtc_connection.on_peer_connection_state_change(Box::new(|s: RTCPeerConnectionState| Box::pin(async move {
             println!("Peer Connection State has changed: {}", s);
 
             if s == RTCPeerConnectionState::Failed {
                 panic!("Peer Connection has gone to failed exiting");
             }
 
-            Box::pin(async {})
-        }))
-        .await;
+        }))).await;
 
-    let d1 = Arc::clone(&data_channel);
+        let signal_client1 = self.signal_client.clone();
 
-    data_channel.on_open(Box::new(move || {
-        println!("Data channel '{}':'{}' open.", d1.label(), d1.id());
+        self.rtc_connection.on_ice_candidate(Box::new(move |candidate| {
+            let signal_client2 = signal_client1.clone();
+            Box::pin(async move {
+                println!("ICE Candidate: {:?}", candidate);
+                if let Some(candidate) = candidate {
+                    signal_client2.clone().send_ice_candidate(candidate.to_json().await.unwrap());
+                }
+            })
+        })).await;
 
-        // let d2 = Arc::clone(&d1);
-        Box::pin(async move {
-            // let mut result = Result::<usize>::Ok(0);
-            // while result.is_ok() {
-            //     let timeout = tokio::time::sleep(Duration::from_secs(5));
-            //     tokio::pin!(timeout);
+        self.rtc_connection.on_data_channel(Box::new(|channel: Arc<RTCDataChannel>| Box::pin(async move {
+            println!("Got data channel '{}':'{}'.", channel.label(), channel.id());
+            channel.on_message(Box::new(|message: DataChannelMessage| Box::pin(async move {
+                println!("Got data channel message: {:?}", message);
+            }))).await;
+            channel.on_error(Box::new(|err| Box::pin(async move {
+                println!("Got data channel error: {:?}", err);
+            }))).await;
+            channel.on_close(Box::new(|| Box::pin(async move {
+                println!("Got data channel close.");
+            }))).await;
+        }))).await;
 
-            //     tokio::select! {
-            //         _ = timeout.as_mut() =>{
-            //             let message = math_rand_alpha(15);
-            //             println!("Sending '{}'", message);
-            //             result = d2.send_text(message).await.map_err(Into::into);
-            //         }
-            //     };
-            // }
-        })
-    })).await;
-
-    // Register text message handling
-    let d_label = data_channel.label().to_owned();
-    data_channel
-        .on_message(Box::new(move |msg: DataChannelMessage| {
-            let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
-            println!("Message from DataChannel '{}': '{}'", d_label, msg_str);
-            Box::pin(async {})
-        }))
-        .await;
-
-    // Create an offer to send to the browser
-    let offer = peer_connection.create_offer(None).await?;
-
-    // Create channel that is blocked until ICE Gathering is complete
-    let mut gather_complete = peer_connection.gathering_complete_promise().await;
-
-    // Sets the LocalDescription, and starts our UDP listeners
-    peer_connection.set_local_description(offer).await?;
-
-    Ok(())
+        let offer = self.rtc_connection.create_offer(None).await?;
+        self.rtc_connection.set_local_description(offer.clone()).await?;
+        self.signal_client.send_session_description(offer);
+        Ok(())
+    }
+    async fn create_data_channel(&mut self, label: &str) -> Res<Arc<RTCDataChannel>> {
+        let channel = self.rtc_connection.create_data_channel(label, None).await?;
+        let channel2 = channel.clone();
+        channel.on_open(Box::new(move || Box::pin(async move {
+            println!("Data channel '{}':'{}' open.", channel2.label(), channel2.id());
+        }))).await;
+        Ok(channel)
+    }
 }
