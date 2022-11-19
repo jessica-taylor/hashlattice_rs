cb8adc19f0d2a370a1a86a1d28d2c4a94b414aea
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-11-18 19:04:22 -0500

    signal server as struct

--- src/transport/signalserver.rs
+++ src/transport/signalserver.rs
@@ -1,6 +1,6 @@
 use std::net::SocketAddr;
 use std::collections::BTreeMap;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
 use futures::{StreamExt};
 
@@ -14,50 +14,56 @@ use crate::transport::signalmessage::{Peer, SignalMessageToClient, SignalMessage
 
 // https://github.com/snapview/tokio-tungstenite/blob/master/examples/autobahn-server.rs
 
-struct SignalServer {
+pub struct SignalServer {
     peer_streams: Mutex<BTreeMap<Peer, WebSocketStream<MaybeTlsStream<TcpStream>>>>,
 }
 
-async fn runserver() -> Res<()> {
-    // let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
-    let addr = "127.0.0.1:2020";
+impl SignalServer {
+    pub fn new() -> Self {
+        let peer_streams = Mutex::new(BTreeMap::new());
+        SignalServer { peer_streams }
+    }
+    pub async fn listen(self: Arc<Self>, addr: &str) -> Res<()> {
+        let addr = "127.0.0.1:2020";
 
-    // Create the event loop and TCP listener we'll accept connections on.
-    let listener = TcpListener::bind(&addr).await?;
-    println!("Listening on: {}", addr);
+        // Create the event loop and TCP listener we'll accept connections on.
+        let listener = TcpListener::bind(&addr).await?;
+        println!("Listening on: {}", addr);
 
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream));
+        while let Ok((stream, _)) = listener.accept().await {
+            tokio::spawn(self.clone().accept_connection(stream));
+        }
+
+        Ok(())
     }
 
-    Ok(())
-}
-
-async fn accept_connection(stream: TcpStream) {
-    let addr = stream.peer_addr().expect("connected streams should have a peer address");
-    println!("Peer address: {}", addr);
+    async fn accept_connection(self: Arc<Self>, stream: TcpStream) {
+        let addr = stream.peer_addr().expect("connected streams should have a peer address");
+        println!("Peer address: {}", addr);
 
 
-    println!("New WebSocket connection: {}", addr);
+        println!("New WebSocket connection: {}", addr);
 
-    if let Err(e) = handle_connection(addr, stream).await {
-        println!("Error handling websocket connection: {}", e);
-    }
-}
-
-async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Res<()> {
-    let mut ws_stream = tokio_tungstenite::accept_async(stream).await?;
-    while let Some(msg) = ws_stream.next().await {
-        match msg? {
-            Message::Binary(bs) => {
-                let msg: SignalMessageToServer = rmp_serde::from_slice(&bs)?;
-                println!("Received a message from {}: {:?}", peer, msg);
-                // ...
-            }
-            other => {
-                println!("Received a message which is not binary: {:?}", other);
-            }
+        if let Err(e) = self.handle_connection(addr, stream).await {
+            println!("Error handling websocket connection: {}", e);
         }
     }
-    Ok(())
+
+    async fn handle_connection(self: Arc<Self>, peer: SocketAddr, stream: TcpStream) -> Res<()> {
+        let mut ws_stream = tokio_tungstenite::accept_async(stream).await?;
+        while let Some(msg) = ws_stream.next().await {
+            match msg? {
+                Message::Binary(bs) => {
+                    let msg: SignalMessageToServer = rmp_serde::from_slice(&bs)?;
+                    println!("Received a message from {}: {:?}", peer, msg);
+                    // ...
+                }
+                other => {
+                    println!("Received a message which is not binary: {:?}", other);
+                }
+            }
+        }
+        Ok(())
+    }
 }
+
