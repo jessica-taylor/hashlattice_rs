b319b813758f182c387d2036e15b7019082aee39
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-09-04 15:44:45 -0400

    arc semilattice

--- hl2/src/lattice.rs
+++ hl2/src/lattice.rs
@@ -1,6 +1,23 @@
+use std::sync::Arc;
+use std::ops::Deref;
 
 pub trait Semilattice : Clone + Send + Sync {
     fn bottom() -> Self;
     fn is_elem(&self) -> Result<(), String>;
     fn join(&mut self, other: &Self) -> Result<(), String>;
+}
+
+impl<L : Semilattice> Semilattice for Arc<L> {
+    fn bottom() -> Self {
+        Arc::new(L::bottom())
+    }
+    fn is_elem(&self) -> Result<(), String> {
+        self.is_elem()
+    }
+    fn join(&mut self, other: &Self) -> Result<(), String> {
+        let mut val: L = self.as_ref().clone();
+        val.join(other.as_ref())?;
+        *self = Arc::new(val);
+        Ok(())
+    }
 }
